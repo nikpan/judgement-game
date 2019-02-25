90c74ec5dafdc0ab50d253c93545f9d7ce2fcc74
Nikhil Pangarkar <nipangar@microsoft.com>
2019-02-25 00:06:49 -0800

    Sort cards in hand

--- judgement-client/src/components/hand.tsx
+++ judgement-client/src/components/hand.tsx
@@ -8,7 +8,7 @@ export interface HandProps {
 
 export default class Hand extends React.Component<HandProps> {
   render() {
-    const cards = this.props.cards;
+    const cards = this.sortCards(this.props.cards);
     return (
       <div>
         <h2>{this.props.name}'s Hand</h2>
@@ -20,11 +20,39 @@ export default class Hand extends React.Component<HandProps> {
   }
 
   private sortCards(cards: CardProps[]) {
-    // TODO: complete sorting logic
-    // cards.sort((a,b) => {
-    //   if (a.rank === b.rank) {
-        
-    //   }
-    // })
+    cards.sort((a,b) => {
+      if (a.suit > b.suit) {
+        return 1;
+      }
+      if(a.suit < b.suit) {
+        return -1;
+      }
+      if(this.rankToValue(a.rank) > this.rankToValue(b.rank)) {
+        return 1;
+      }
+      if(this.rankToValue(a.rank) < this.rankToValue(b.rank)) {
+        return -1
+      }
+      return 0;
+    })
+    return cards;
+  }
+
+  private rankToValue(rank: string): number {
+    switch (rank) {
+      case 'J':
+        return 11;
+      case 'Q':
+        return 12;
+      case 'K':
+        return 13;
+      case 'A':
+        return 14;
+      default:
+        const val = parseInt(rank);
+        if (val >= 2 && val <= 10) return val;
+        return -1
+    }
+    
   }
 }
