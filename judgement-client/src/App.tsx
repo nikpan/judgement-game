4d3d7c5f64ac3c70ac89a6e663796cb6621edba0
Nikhil Pangarkar <nipangar@microsoft.com>
2019-02-26 22:46:27 -0800

    Convert App.js to tsx

--- judgement-client/src/App.tsx
+++ judgement-client/src/App.tsx
@@ -2,29 +2,47 @@ import React from 'react';
 import './App.css';
 import Hand from './components/hand'
 import HiddenHand from './components/hiddenHand';
+import { CardProps } from './components/card';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      webSocket: null,
-      name: 'Nikhil',
-      myCards: [],
-      otherPlayers: []
-    };
-    this.onSetNameClick = this.onSetNameClick.bind(this);
-    this.onDealClick = this.onDealClick.bind(this);
-    this.onCardClick = this.onCardClick.bind(this);
-  }
+export interface AppState {
+  webSocket: WebSocket | null;
+  name: string;
+  myCards: CardProps[];
+  otherPlayers: PlayerInfo[];
+}
+
+enum MessageType {
+  Deal = 'Deal',
+  Join = 'Join',
+  Hand = 'Hand',
+  AllPlayers = 'AllPlayers'
+}
+
+interface PlayerInfo {
+  name: string;
+  cardCount: number;
+}
+
+interface ServerMessage {
+  action: MessageType;
+  cards?: CardProps[];
+  players?: PlayerInfo[];
+}
+
+class App extends React.Component<{},AppState> {
+  initialState = {
+    webSocket: null,
+    name: 'Nikhil',
+    myCards: [],
+    otherPlayers: []
+  };
 
   componentDidMount() {
   }
 
-  openConnection() {
+  openConnection = () => {
     const ws = new WebSocket('ws://localhost:3001');
-    this.setState((prevState, props) => {
-      return { webSocket: ws }
-    });
+    this.setState({webSocket: ws});
     ws.onopen = () => {
       console.debug('Connection established!');
       var data = {
@@ -34,14 +52,14 @@ class App extends React.Component {
       ws.send(JSON.stringify(data));
     };
     ws.onmessage = (msg) => {
-      var msgData = JSON.parse(msg.data);
-      if (msgData.action === 'Hand') {
+      var msgData: ServerMessage = JSON.parse(msg.data);
+      if (msgData.action === 'Hand' && msgData.cards) {
         var cardsFromServer = msgData.cards;
         this.setState({ myCards: cardsFromServer });
       }
-      if (msgData.action === 'AllPlayers') {
+      if (msgData.action === 'AllPlayers' && msgData.players) {
         let otherPlayerInfos = msgData.players;
-        let toRemove = otherPlayerInfos.findIndex(player => player.name === this.state.name);
+        let toRemove = otherPlayerInfos.findIndex((player: { name: string; }) => player.name === this.state.name);
         otherPlayerInfos.splice(toRemove, 1);
         this.setState({ otherPlayers: otherPlayerInfos });
       }
@@ -49,34 +67,36 @@ class App extends React.Component {
     };
   }
 
-  closeConnection() {
-    if (this.state.webSocket) {
+  closeConnection = () => {
+    if (this.state.webSocket != null) {
       this.state.webSocket.close();
     }
   }
 
-  handleChange(event) {
+  handleChange = (event: any) => {
     this.setState({ name: event.target.value });
   }
 
-  onSetNameClick() {
+  onSetNameClick = () => {
     this.closeConnection();
     this.openConnection();
   }
 
-  onDealClick() {
+  onDealClick = () => {
     var data = {
       action: 'Deal'
     };
-    this.state.webSocket.send(JSON.stringify(data));
+    if (this.state.webSocket != null) {
+      this.state.webSocket.send(JSON.stringify(data));
+    }
   }
 
-  onCardClick(suit, rank){
+  onCardClick = (suit: string, rank: string) => {
     console.log(suit + rank);
   }
 
   render() {
-    let otherPlayers = [];
+    let otherPlayers:any[] = [];
     this.state.otherPlayers.forEach(player => {
       otherPlayers.push(<HiddenHand name={player.name} cardCount={player.cardCount}></HiddenHand>)
     });
@@ -92,7 +112,11 @@ class App extends React.Component {
         <button onClick={this.onSetNameClick}>Submit</button>
         <button onClick={this.onDealClick}>Deal!</button>
         <div>
-          <Hand name={this.state.name} cards={this.state.myCards} cardSelected={this.onCardClick}></Hand>
+          <Hand 
+            name={this.state.name} 
+            cards={this.state.myCards} 
+            cardSelected={this.onCardClick}
+          />
           {otherPlayers}
         </div>
       </div>
